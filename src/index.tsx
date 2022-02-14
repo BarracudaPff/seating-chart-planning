056e0a00b51b7a094701cc10b3422b88474be937
BarracudaPff <mrm.kikll@gmail.com>
2022-02-14 19:02:50 +0300

    Revert "save"
    This reverts commit c869fcd8d750be5f55aa56f49e8201e42c2c1c88.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,7 @@
 import * as React from "react";
-import {render} from "react-dom";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { render } from "react-dom";
 import App from "./components/App";
-import MainRouter from "./routes/main";
 
 const rootEl = document.getElementById("root");
 
-render(
-    <MainRouter/>,
-    // <App />,
-    // <BrowserRouter>
-    //     <Routes>
-    //         <Route path="/" element={<App />} />
-    //         {/*<Route path="expenses" element={<Expenses />} />*/}
-    //         {/*<Route path="invoices" element={<Invoices />} />*/}
-    //     </Routes>
-    // </BrowserRouter>,
-    rootEl
-);
+render(<App />, rootEl);
