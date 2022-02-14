c869fcd8d750be5f55aa56f49e8201e42c2c1c88
BarracudaPff <mrm.kikll@gmail.com>
2022-02-14 19:02:39 +0300

    save

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,18 +1,23 @@
 // development config
-const { merge } = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const commonConfig = require("./common");
+const {addReactRefresh} = require('customize-cra-react-refresh')
 
-module.exports = merge(commonConfig, {
-  mode: "development",
-  entry: [
-    "react-hot-loader/patch", // activate HMR for React
-    "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
-    "./index.tsx", // the entry point of our app
-  ],
-  devServer: {
-    hot: "only", // enable HMR on the server
-    historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url 
-  },
-  devtool: "cheap-module-source-map",
-  plugins: [],
+module.exports = merge(commonConfig, addReactRefresh(), {
+    mode: "development",
+    entry: [
+        // "react-hot-loader/patch", // activate HMR for React
+        "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
+        "./index.tsx", // the entry point of our app
+        // "webpack/hot/dev-server.js",
+    ],
+    output: {
+        publicPath: '/'
+    },
+    devServer: {
+        hot: "only", // enable HMR on the server
+        historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
+    },
+    devtool: "cheap-module-source-map",
+    plugins: [],
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,40 @@
 import * as React from "react";
-import { hot } from "react-hot-loader";
+import {ChangeEvent, useState} from "react";
+import {Button} from "antd";
+import "./../assets/scss/App.scss";
+import {Link} from "react-router-dom";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
 
-class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
+const App = () => {
+    const [input, setInput] = useState<string>("");
+
+    const onInput = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        // console.log({zlib: zlib.inflateSync})
+        const value = event.target.value
+        setInput(value);
+    }
+
     return (
-      <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
+        <div className="app">
+            <h1>DDD</h1>
+            <p>Foo 1to11 the barz</p>
+            <img src={reactLogo.default} height="480"/>
+            <div>
+                <textarea className={"long-input"} value={input} onChange={onInput}/>
+            </div>
+            <div className={"ad"}>
+                {input}
+            </div>
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+            <nav>
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+        </div>
+    )
 }
 
-declare let module: Record<string, unknown>;
-
-export default hot(module)(App);
+export default App;
