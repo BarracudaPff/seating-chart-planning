df0cf6cd4d1872b5840fb503f99d7dea7c4a4595
BarracudaPff <mrm.kikll@gmail.com>
2022-02-18 04:46:34 +0300

    add components

---
+++ src/constants/storage.ts
@@ -0,0 +1,7 @@
+export enum STORAGE_KEYS {
+    User = "User",
+    SidebarCollapsed = "SIDEBAR_COLLAPSED",
+    LoginName = "LOGIN_NAME",
+    LockScreen = "LOCK_SCREEN",
+    CompanyId = "COMPANY_ID"
+}
